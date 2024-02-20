df80d53c3235a3deae2d3b8fc0355c71edf4a520
Taurus <gongyanhui@eagleyun.com>
2024-02-20 11:35:59 +0800

    [feat]:贷款统计and计算

---
+++ GoStudy/something_test/my_principal.go
@@ -0,0 +1,62 @@
+/**
+ * @Author: Gong Yanhui
+ * @Description: 贷款利息计算
+ * @Date: 2024-02-20 11:17
+ */
+
+package something_test
+
+import "fmt"
+
+func Calculate() {
+	totalPrincipal, ms := getMoney()
+	var (
+		total     int
+		principal int
+		interest  int
+	)
+
+	for _, m := range ms {
+		if !m.check() {
+			panic(fmt.Sprintf("数据错误:%+v", m))
+		}
+		total += m.Total
+		principal += m.Principal
+		interest += m.Interest
+	}
+
+	if principal != totalPrincipal {
+		panic(fmt.Sprintf("数据错误: principal != totalPrincipal principal:%f,totalPrincipal:%f", principal, totalPrincipal))
+	}
+
+	if total != totalPrincipal+interest {
+		panic(fmt.Sprintf("数据错误: total != totalPrincipal+interest total:%f,totalPrincipal:%f,interest:%f", total, totalPrincipal, interest))
+	}
+	fmt.Printf("total:%d,principal:%d,interest:%d", total, principal, interest)
+}
+
+func getMoney() (int, []Money) {
+	// 2023
+	var totalPrincipal int = 2619103
+	var ms = []Money{
+		{Total: 1707023, Principal: 369398, Interest: 1337625}, // 7月
+		{Total: 1261148, Principal: 370660, Interest: 890488},  // 8月
+		{Total: 1261148, Principal: 371927, Interest: 889221},  // 9月
+		{Total: 1261148, Principal: 373197, Interest: 887951},  // 10月
+		{Total: 1261148, Principal: 374472, Interest: 886676},  // 11月
+		{Total: 1261148, Principal: 375752, Interest: 885396},  // 12月
+		{Total: 1267809, Principal: 383697, Interest: 884112},  // 1月
+	}
+
+	return totalPrincipal, ms
+}
+
+type Money struct {
+	Total     int
+	Principal int
+	Interest  int
+}
+
+func (m Money) check() bool {
+	return m.Total == m.Principal+m.Interest
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,11 @@ require (
 )
 
 require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/shirou/gopsutil v2.21.11+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.13 // indirect
+	github.com/tklauser/numcpus v0.7.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee // indirect
-	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 )
