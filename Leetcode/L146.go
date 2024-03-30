43ba5bededcf7ebab67f89ccf4a5ffafd30c5dcc
Taurus <gongyanhui@eagleyun.com>
2024-03-30 13:21:42 +0800

    [feat]:146. LRU 缓存

---
+++ Leetcode/L146.go
@@ -0,0 +1,91 @@
+/**
+ * @Author: Gong Yanhui
+ * @Description: 146. LRU 缓存
+ * @Date: 2024-03-30 12:41
+ */
+
+package main
+
+type LRUCache struct {
+	capacity, size int
+	cache          map[int]*DLinkedNode
+	head, tail     *DLinkedNode
+}
+
+func LRUCacheConstructor(capacity int) LRUCache {
+	var l = LRUCache{
+		capacity: capacity,
+		cache:    make(map[int]*DLinkedNode),
+		head:     initDLinkedNode(0, 0),
+		tail:     initDLinkedNode(0, 0),
+	}
+	l.head.next = l.tail
+	l.tail.prev = l.head
+	return l
+}
+
+func (this *LRUCache) Get(key int) int {
+	if _, ok := this.cache[key]; !ok {
+		return -1
+	}
+	node := this.cache[key]
+	this.moveToHead(node)
+	return node.value
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if _, ok := this.cache[key]; !ok {
+		node := initDLinkedNode(key, value)
+		this.cache[key] = node
+		this.addToHead(node)
+		this.size++
+		if this.size > this.capacity {
+			removed := this.removeTail()
+			delete(this.cache, removed.key)
+			this.size--
+		}
+	} else {
+		node := this.cache[key]
+		node.value = value
+		this.moveToHead(node)
+	}
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
+
+type DLinkedNode struct {
+	key, value int
+	prev, next *DLinkedNode
+}
+
+func initDLinkedNode(key, value int) *DLinkedNode {
+	return &DLinkedNode{key: key, value: value}
+}
+
+func (this *LRUCache) addToHead(node *DLinkedNode) {
+	node.prev = this.head
+	node.next = this.head.next
+	this.head.next.prev = node
+	this.head.next = node
+}
+
+func (this *LRUCache) removeNode(node *DLinkedNode) {
+	node.prev.next = node.next
+	node.next.prev = node.prev
+}
+
+func (this *LRUCache) moveToHead(node *DLinkedNode) {
+	this.removeNode(node)
+	this.addToHead(node)
+}
+
+func (this *LRUCache) removeTail() *DLinkedNode {
+	node := this.tail.prev
+	this.removeNode(node)
+	return node
+}
