8d0a296cf3cc1c2c748a7bb325032a8f7d196ebe
Taurus <gongyanhui@eagleyun.com>
2024-06-17 14:26:27 +0800

    [feat]:WhatIsMyIP

---
+++ GoStudy/something_test/whatismyip.go
@@ -0,0 +1,28 @@
+/**
+ * @Author: Gong Yanhui
+ * @Description: http服务 返回客户端的IP地址
+ * @Date: 2024-06-17 14:24
+ */
+
+package something_test
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+func WhatIsMyIP() {
+
+	pListenPort := flag.String("p", "8080", "LISTEN PORT")
+	flag.Parse()
+
+	// 启动http服务
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("YOUR IP: " + r.RemoteAddr))
+	})
+	err := http.ListenAndServe(fmt.Sprintf(":%s", *pListenPort), nil)
+	if err != nil {
+		fmt.Println("http server start failed, err:", err)
+	}
+}
