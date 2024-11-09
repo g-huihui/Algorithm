3f91a640e1807a4d534ca4a7cc96a465b1646f1e
Taurus <gongyanhui@eagleyun.com>
2024-11-09 14:46:42 +0800

    [feat]:获取文件修改时间

---
+++ GoStudy/file/file_time.go
@@ -0,0 +1,41 @@
+/**
+ * @Author: Gong Yanhui
+ * @Description: 获取文件
+ * @Date: 2024-11-09 14:31
+ */
+
+package file
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func GetFileModTime() {
+	file, err := os.Open("/Users/gongyanhui/GolandProjects/Algorithm/GoStudy/file/test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(fileInfo.ModTime())
+	fmt.Println(fileInfo.ModTime().Unix())
+	fmt.Println(fileInfo.ModTime().Format("2006-01-02 15:04:05"))
+}
+
+func GetFileModTimeSimple() {
+	fileInfo, err := os.Stat("/Users/gongyanhui/GolandProjects/Algorithm/GoStudy/file/test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fileInfo.ModTime())
+	fmt.Println(fileInfo.ModTime().Unix())
+	fmt.Println(fileInfo.ModTime().Format("2006-01-02 15:04:05"))
+}
